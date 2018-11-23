7eeb50ead6f579b9c93db089a5a9c9651e998977
itenpn <iten.pn@gmail.com>
2018-11-22 21:29:55 -0500

    Angular Update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { Body1Component } from './body1/body1.component';
+import { Body2Component } from './body2/body2.component';
+import { AboutmeComponent } from './aboutme/aboutme.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Body1Component,
+    Body2Component,
+    AboutmeComponent,
   ],
   imports: [
     BrowserModule
